8bf8d207dd947d2359204be2896bf483da96d886
Igor <oneseq@gmail.com>
2019-07-04 00:09:30 +0300

    Create

---
+++ iShop/iShop/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iShop
+//
+//  Created by Igor on 03/07/2019.
+//  Copyright © 2019 Igorlab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
