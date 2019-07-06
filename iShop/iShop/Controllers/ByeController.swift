aed5a9468eaf8096fd8734a692f09cce88376b80
Igor <oneseq@gmail.com>
2019-07-06 21:30:02 +0300

    update

--- iShop/iShop/Controllers/ByeController.swift
+++ iShop/iShop/Controllers/ByeController.swift
@@ -8,20 +8,13 @@
 
 import UIKit
 
+extension Notification.Name {
+    static let reload = Notification.Name("reload")
+}
+
 class ByeController: UITableViewController {
     
     var productList2 = [Product]()
-    
-    public var productList = [
-        Product(id: 1, name: "Prod 1", image: "img", info: "Product is good", price: 123),
-        Product(id: 2, name: "Prod 2", image: "img", info: "Product is good", price: 23),
-        Product(id: 3, name: "Prod 3", image: "img", info: "Product is good", price: 45),
-        Product(id: 4, name: "Prod 4", image: "img", info: "Product is good", price: 44),
-        Product(id: 5, name: "Prod 5", image: "img", info: "Product is good", price: 456),
-        Product(id: 6, name: "Prod 6", image: "img", info: "Product is good", price: 5657),
-        Product(id: 7, name: "Prod 7", image: "img", info: "Product is good", price: 4535),
-        Product(id: 8, name: "Prod 8", image: "img", info: "Product is good", price: 124677)
-    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,9 +26,17 @@ class ByeController: UITableViewController {
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
         
         readFromJSON()
-        
-    }
+//        updater()
+        NotificationCenter.default.addObserver(self, selector: #selector(reloadData), name: .reload, object: nil)
 
+    }
+    
+    @objc func reloadData(_ notification : Notification) {
+        readFromJSON()
+        tableView.reloadData()
+    }
+    
+    
     // MARK: - Table view data source
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -57,52 +58,6 @@ class ByeController: UITableViewController {
     }
     
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-    
     func readFromJSON() {
         let jsonPath = Bundle.main.path(forResource: "ProductDB2", ofType: "json")
         print(jsonPath!)
@@ -127,19 +82,22 @@ class ByeController: UITableViewController {
         
         print(productList2)
     }
-    /*
+    
     func updater() {
-        let timer = Timer.init(timeInterval: 5, repeats: true) { timer in
-//            timer.invalidate()
-            print("Autoupdate!")
+//        let timer = Timer.scheduledTimer(withTimeInterval: 2.0, repeats: true) { timer in
+//            self.readFromJSON()
+//            self.tableView.reloadData()
+//            print("Timer: \(timer) - fired! \(Date.self))")
+//        }
+
+        let timer = Timer.scheduledTimer(withTimeInterval: 2, repeats: true) { timer in
+            self.readFromJSON()
+            self.tableView.reloadData()
+            print("Timer: \(timer) - working! \(Date().description))")
         }
-        DispatchQueue.global().async {
-            Timer.init(timeInterval: 1, repeats: true, block: { timer in
-                print(timer.fireDate)
-                DispatchQueue.main.async {
-                    self.tableView.reloadData()
-                }
-            })
-        }
-    }*/
+
+    }
+    
 }
+
+
--- iShop/iShop/Controllers/SaleController.swift
+++ iShop/iShop/Controllers/SaleController.swift
@@ -13,100 +13,97 @@ class SaleController: UIViewController {
     @IBOutlet weak var addProdNameLabel: UITextField!
     @IBOutlet weak var addProdInfoText: UITextView!
     @IBOutlet weak var addProdPriceLabel: UITextField!
+    @IBOutlet weak var addButton: UIButton!
     
-    var backgroundColor = 0
-
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        addProdInfoText.bounces = true
-        
+        configureInfoTextView()
         
         // Do any additional setup after loading the view.
         
         let tapGR = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
         view.addGestureRecognizer(tapGR)
+        
+        
     }
     
     @objc func hideKeyboard() {
         view.endEditing(true)
         
     }
-
-    /*
+    
     // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
     
     @IBAction func addProductButton(_ sender: Any) {
         
         if addProdInfoText.text.isEmpty || addProdPriceLabel.text!.isEmpty || addProdNameLabel.text!.isEmpty {
+            
             print("NO! ❌❌‼️‼️ ")
-        }
+        } else {
+            DispatchQueue.global().async {
+                sleep(5)
                 
-        DispatchQueue.global().async {
-            sleep(5)
-            
-            if self.backgroundColor == 0 {
-                DispatchQueue.main.async {
-                    self.view.backgroundColor = .green
+                var lastID = 0
+                var newJSONDB: JSONDB
+                
+                let jsonPath = Bundle.main.path(forResource: "ProductDB2", ofType: "json")
+                let url = URL(fileURLWithPath: jsonPath!)
+                let data = try! Data(contentsOf: url)
+                
+                do {
+                    let db = try JSONDecoder().decode(JSONDB.self, from: data)
+                    let count = db.products.count
+                    lastID = db.products[count - 1].id
+                    newJSONDB = db
+                    print("\(db)  🎈")
+                    
+                    let id = lastID + 1
+                    let name = self.addProdNameLabel.text!
+                    let image = "img"
+                    let info = self.addProdInfoText.text!
+                    let textPrice = self.addProdPriceLabel.text!
+                    let price = Int(textPrice) ?? 0
+                    
+                    let newProduct = Product(id: id, name: name, image: image, info: info, price: price)
+                    print(newProduct)
+                    newJSONDB.products.append(newProduct)
+                    print(newJSONDB.products)
+                    
+                    let encoder = JSONEncoder()
+                    do {
+                        let jsonData = try! encoder.encode(newJSONDB)
+                        let jsonString = String(data: jsonData, encoding: .utf8)
+                        print("\(jsonData) : \(String(describing: jsonString))")
+                        try jsonData.write(to: url)
+                    } catch {
+                        print("Failed to write JSON data: \(error.localizedDescription)")
+                    }
+                    
+                } catch {
+                    print("error 🎈")
                 }
                 
-                self.backgroundColor = 1
-            } else {
                 DispatchQueue.main.async {
-                    self.view.backgroundColor = .white
+                    NotificationCenter.default.post(name: .reload, object: nil)
                 }
-                self.backgroundColor = 0
-            }
-        }
-        
-        var lastID = 0
-        var newJSONDB: JSONDB
-        
-        let jsonPath = Bundle.main.path(forResource: "ProductDB2", ofType: "json")
-        let url = URL(fileURLWithPath: jsonPath!)
-        let data = try! Data(contentsOf: url)
-        
-        do {
-            let db = try JSONDecoder().decode(JSONDB.self, from: data)
-            let count = db.products.count
-            lastID = db.products[count - 1].id
-            newJSONDB = db
-            print("\(db)  🎈")
-            
-            let id = lastID + 1
-            let name = addProdNameLabel.text!
-            let image = "img"
-            let info = addProdInfoText.text!
-            let textPrice = addProdPriceLabel.text!
-            let price = Int(textPrice) ?? 0
-            
-            let newProduct = Product(id: id, name: name, image: image, info: info, price: price)
-            print(newProduct)
-            newJSONDB.products.append(newProduct)
-            print(newJSONDB.products)
-            
-            let encoder = JSONEncoder()
-            do {
-                let jsonData = try! encoder.encode(newJSONDB)
-                let jsonString = String(data: jsonData, encoding: .utf8)
-                print("\(jsonData) : \(String(describing: jsonString))")
-                try jsonData.write(to: url)
-            } catch {
-                print("Failed to write JSON data: \(error.localizedDescription)")
             }
             
-        } catch {
-            print("error 🎈")
         }
+                
         
+        
+
     }
     
+    func configureInfoTextView() {
+        let borderColor = UIColor.lightGray.cgColor.copy(alpha: 0.25)
+        
+        addProdInfoText.layer.borderWidth = 1
+        addProdInfoText.layer.borderColor = borderColor
+        addProdInfoText.layer.cornerRadius = 5
+    }
 
 }
+
+
